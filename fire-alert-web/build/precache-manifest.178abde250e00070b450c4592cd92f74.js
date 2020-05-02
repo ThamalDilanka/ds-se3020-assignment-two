b3f9be5bc24570151e714ddffcec43b74e04f150
Thamal Dilanka <thamaldilanke@gmail.com>
2020-05-02 17:26:15 +0530

    Web client binaries added

---
+++ fire-alert-web/build/precache-manifest.178abde250e00070b450c4592cd92f74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2d3fe96cf4a106b177e179873a9e623",
+    "url": "/fire-alert-web/index.html"
+  },
+  {
+    "revision": "8b090b67c3754d61e60e",
+    "url": "/fire-alert-web/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "38d79660b274782f3cb8",
+    "url": "/fire-alert-web/static/css/main.1672f68a.chunk.css"
+  },
+  {
+    "revision": "8b090b67c3754d61e60e",
+    "url": "/fire-alert-web/static/js/2.8b002ed0.chunk.js"
+  },
+  {
+    "revision": "b0833967fff6815957361c02ceb48c9a",
+    "url": "/fire-alert-web/static/js/2.8b002ed0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38d79660b274782f3cb8",
+    "url": "/fire-alert-web/static/js/main.57f62816.chunk.js"
+  },
+  {
+    "revision": "6db3d8e6f26fa6d77afb",
+    "url": "/fire-alert-web/static/js/runtime-main.fd21e1fa.js"
+  }
+]);
